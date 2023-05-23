722e30b6ab2d0dba2109ba68171eeafaa1464ccf
Gabriel S <thesouzagab@gmail.com>
2023-05-23 15:26:32 -0300

    chore: comment action_text import

--- config/application.rb
+++ config/application.rb
@@ -9,10 +9,9 @@ require "active_storage/engine"
 require "action_controller/railtie"
 require "action_mailer/railtie"
 # require "action_mailbox/engine"
-require "action_text/engine"
+# require "action_text/engine"
 require "action_view/railtie"
 require "action_cable/engine"
-# require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
